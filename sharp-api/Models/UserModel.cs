437b25652bf0ce1132319f98b944c6f796616ac7
Aimar Karrik <71828910+AimarKarrik@users.noreply.github.com>
2024-11-26 19:21:41 +0200

    Add project files.

---
+++ sharp-api/Models/UserModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sharp_api.Models
+{
+    public class UserModel
+    {
+        public string Id { get; }
+        public string Name { get; }
+        public string Email { get; }
+        public string Phone {  get; } 
+    }
+}
