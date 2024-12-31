930d58b033b4322b30edbe23c8a331c4c0845e9a
Aimar Karrik <71828910+AimarKarrik@users.noreply.github.com>
2024-12-31 11:08:38 +0200

    A new start

--- Models/UserModel.cs
+++ Models/UserModel.cs
@@ -4,17 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace sharp_api.Models
+namespace Sharp_Api.Models
 {
     public class UserModel
     {
         public string user_id { get; set; }
         public string name { get; set; }
-        public string phone { get; set; } 
+        public string phone { get; set; }
         public string email { get; set; }
-    }
-    public class UserListModel
-    {
-        public List<UserModel> users { get; set;}
     }
 }
