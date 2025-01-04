78e0ce6fdda4efcf5372ad35f3fc37962501be60
Aimar Karrik <71828910+AimarKarrik@users.noreply.github.com>
2025-01-04 20:09:06 +0200

    Created the first endpoint (a great success)

---
+++ Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Hosting.Server;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Sharp_Api.Helpers;
+using Sharp_Api.Models;
+
+namespace Sharp_Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+
+
+        [HttpGet(Name = "GetUsers")]
+        public List<UserModel>? Get()
+        {
+            List<UserModel>? users = UserHelper.GetUserData();
+            if (users == null)
+            { 
+                return null;
+            }
+            return users;
+        }
+    }
+}
---
+++ Helpers/UserHelper.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using Sharp_Api.Models;
+
+namespace Sharp_Api.Helpers
+{
+    public class UserHelper
+    {
+
+        
+
+        public static List<UserModel>? GetUserData()
+        {
+            List<UserModel>? users; 
+            try
+            {
+                string jsonData = File.ReadAllText("./DemoData/User.json");
+                users = JsonConvert.DeserializeObject<List<UserModel>>(jsonData);
+            }
+            catch 
+            {
+                return null;
+            }
+
+            return users;
+        }
+    }
+}
